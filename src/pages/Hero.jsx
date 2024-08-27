4e155a91612cd96cbfd7f309233b3e0e8e5fd535
Reynaldo <reynaldobocaling@gmail.com>
2024-08-27 21:17:06 +0800

    upload

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -13,24 +13,11 @@ const Hero = () => {
             YIN YANG
           </h1>
           <h1
-            className="text-stroke-lg text-shadow-lg font-title text-3xl md:text-8xl text-white z-30 mb-12 -mt-4
+            className="text-stroke-lg text-shadow-lg font-title text-3xl md:text-8xl text-white z-30 mb-12
           text-center"
           >
             The balance between Chads and Jeets.
           </h1>
-
-          <h1 className="text-stroke-sm text-shadow-md font-title text-3xl text-white z-30">
-            CONTRACT ADDRESS
-          </h1>
-          <p></p>
-          <div className="flex items-center gap-3 bg-red-10 py-3 pl-7 pr-2 rounded-full box-shadow-sm border border-black">
-            <p className="text-sm md:text-xl text-white font-title tracking-wider">
-              0X8DHS878DFS7DSNSDS8DSYDS78s8s7
-            </p>
-            <button className="btn text-sm md:text-lg font-title text-black py-2 px-5 rounded-full">
-              COPY
-            </button>
-          </div>
         </div>
       </div>{" "}
       <DottedBackground />
